2bfedb43bd1fce039f32b43272c53431e4e795a5
demamama <82538255+demamama@users.noreply.github.com>
2021-04-14 23:21:45 +0300

    Add files via upload

---
+++ cube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private Rigidbody _rigidBody;
+    private void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "tape")
+        {
+            _rigidBody.AddForce(Vector3.forward * _speed);
+        }
+    }
+}
